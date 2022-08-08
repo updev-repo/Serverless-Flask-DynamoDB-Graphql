8574ceac5fb9c8f51cfe68f476ce1578d8d181a9
devStar <devStar7@outlook.com>
2022-08-08 19:07:48 +0200

    formatted the code

--- app/mutations/create_user.py
+++ app/mutations/create_user.py
@@ -2,14 +2,17 @@ import graphene
 from app.typedefs.UserType import UserType
 from app.model import UserModel
 
+
 class CreateUser(graphene.Mutation):
-   class Arguments:
-       userId = graphene.String(required=True)
-       name = graphene.String(required=True)
-       email = graphene.String(required=True)
-   user = graphene.Field(lambda: UserType)   
-   def mutate(self, info, userId, name, email):
-       user = UserModel(userId=userId, name=name, email=email)
-       user.save()
-       
-       return CreateUser(user=user)
+    class Arguments:
+        userId = graphene.String(required=True)
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    user = graphene.Field(lambda: UserType)
+
+    def mutate(self, info, userId, name, email):
+        user = UserModel(userId=userId, name=name, email=email)
+        user.save()
+
+        return CreateUser(user=user)
